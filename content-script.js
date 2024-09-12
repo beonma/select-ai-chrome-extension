c531cb05fe2bb71bc93a94deca8826b405a840a6
hichamelb <hichamelb54@gmail.com>
2024-09-12 02:58:21 +0100

    add HTML element that dynamically aligns with the user's selected text

--- content-script.js
+++ content-script.js
@@ -1,1 +1,27 @@
-alert("im here content");
+const selection = getSelection();
+
+const html = `
+<div class="i1U2I__container">
+    <button>rephrase me</button>
+</div>
+`;
+
+document.querySelector("body").insertAdjacentHTML("afterbegin", html);
+
+const htmlNode = document.querySelector(".i1U2I__container");
+
+document.addEventListener("mouseup", () => {
+    if (selection.type !== "Range") {
+        console.log("nothing to select");
+
+        return;
+    }
+
+    const range = selection.getRangeAt(0);
+
+    console.log(range.toString());
+    const { bottom: offsetY, left: offsetX } = range.getBoundingClientRect();
+
+    htmlNode.style.top = `${offsetY + window.scrollY + 10}px`;
+    htmlNode.style.left = `${offsetX}px`;
+});
