bb245f959a33df43292715ff02e3a6a1312d802a
hichamelb <hichamelb54@gmail.com>
2024-10-16 23:53:28 +0100

    fix: improper behavior when selecting text in input elements

--- src/content/index.ts
+++ src/content/index.ts
@@ -5,11 +5,17 @@ import CaretSVG from "../assets/svg/caret.svg";
 import SpellingSVG from "../assets/svg/spelling.svg";
 import LoadingSVG from "../assets/svg/loading.svg";
 
-const selection = getSelection();
 let selectionTimeout: ReturnType<typeof setTimeout>;
-let clonedRange: Range;
+let selectionRef: {
+    parent: HTMLElement | HTMLInputElement | HTMLTextAreaElement;
+    isInput: boolean;
+    start: number;
+    end: number;
+    text: string;
+};
 
 const rephraseTones = ["formal", "convince", "normal", "angry"];
+const buttons = ["accept", "discard", "try again"];
 
 const html = `
 <div class="${styles.container}">
@@ -32,9 +38,7 @@ const html = `
     <div class="${styles.content}">
         <p></p>
         <div class="${styles.action__container}">
-            <button class="${styles.btn}">accept</button>
-            <button class="${styles.btn}">discard</button>
-            <button class="${styles.btn}">try again</button>
+            ${buttons.map(btn => `<button class="${styles.btn}">${btn}</button>`).join("")}
         </div>
     </div>
 </div>
@@ -43,53 +47,100 @@ const html = `
 document.querySelector("body")?.insertAdjacentHTML("afterbegin", html);
 
 const htmlNode = <HTMLElement>document.querySelector(`.${styles.container}`);
+const editableElements: NodeListOf<HTMLTextAreaElement | HTMLInputElement> =
+    document.querySelectorAll("input, textarea");
 
-const rephraseBtn = <HTMLButtonElement>(
-    htmlNode.querySelector(`.${styles.toolbar} button:first-child`)
-);
-const fixSpellingBtn = <HTMLButtonElement>(
-    htmlNode.querySelector(`.${styles.toolbar} button:last-child`)
-);
+const rephraseBtn = <HTMLButtonElement>htmlNode.querySelector(`.${styles.toolbar} button:first-child`);
+const fixSpellingBtn = <HTMLButtonElement>htmlNode.querySelector(`.${styles.toolbar} button:last-child`);
 const rephraseSelect = <HTMLSelectElement>htmlNode.querySelector(`.${styles.toolbar} select`);
 
 const content = <HTMLDivElement>htmlNode.querySelector(`.${styles.content}`);
 const contentParagraph = <HTMLParagraphElement>content.querySelector("p");
 
-const acceptButton = <HTMLButtonElement>(
-    content.querySelector(`.${styles.action__container} button:first-child`)
-);
-const discardButton = <HTMLButtonElement>(
-    content.querySelector(`.${styles.action__container} button:nth-child(2)`)
-);
-const tryAgainButton = <HTMLButtonElement>(
-    content.querySelector(`.${styles.action__container} button:last-child`)
-);
+const acceptButton = <HTMLButtonElement>content.querySelector(`.${styles.action__container} button:first-child`);
+const discardButton = <HTMLButtonElement>content.querySelector(`.${styles.action__container} button:nth-child(2)`);
+const tryAgainButton = <HTMLButtonElement>content.querySelector(`.${styles.action__container} button:last-child`);
 
 document.addEventListener("click", event => {
     // COMMENT typescript forcing as Node in event.target
-    if (htmlNode.contains(event.target as Node) || selection?.type === "Range") {
+    // TODO To hide the toolbar, up to two clicks are required within the parent element of the currently selected text.
+    if (htmlNode.contains(event.target as Node)) {
+        window.getSelection()?.removeAllRanges();
+        return;
+    }
+
+    if (window.getSelection()?.type === "Range") {
         return;
     }
 
     hideToolbar();
 });
 
+document.addEventListener("keydown", e => {
+    if (e.code === "Escape" || window.getSelection()?.type !== "Range") {
+        hideToolbar();
+    }
+});
+
+editableElements.forEach(el => {
+    el.addEventListener("select", event => {
+        // COMMENT The event continues to trigger even after interacting with the toolbar element (absolute element), despite no changes in the text selection.
+        // INFO no need to add clearTimeout(selectionTimeout), it gets called every time in selectionchange listener
+
+        const target = event.target as HTMLInputElement | HTMLTextAreaElement;
+
+        if (
+            typeof target.selectionStart !== "number" ||
+            typeof target.selectionEnd !== "number" ||
+            (target.selectionStart === 0 && target.selectionEnd === 0)
+        ) {
+            return;
+        }
+
+        const selectionStart = target.selectionStart;
+        const selectionEnd = target.selectionEnd;
+        const selection = target.value.substring(selectionStart, selectionEnd);
+
+        selectionTimeout = setTimeout(() => {
+            showToolbar(target.getBoundingClientRect());
+
+            selectionRef = {
+                text: selection,
+                parent: target,
+                start: selectionStart,
+                end: selectionEnd,
+                isInput: true,
+            };
+        }, 500);
+    });
+});
+
 document.addEventListener("selectionchange", () => {
     clearTimeout(selectionTimeout);
+
+    const selection = window.getSelection();
 
     if (selection?.type !== "Range") {
         return;
     }
 
+    const range = selection.getRangeAt(0);
+    const parentElement = range.startContainer.parentElement;
+
+    if (range.toString() === "" || !parentElement) {
+        return;
+    }
+
     selectionTimeout = setTimeout(() => {
-        const range = selection.getRangeAt(0);
-        const { bottom: offsetY, left: offsetX } = range.getBoundingClientRect();
+        showToolbar(range.getBoundingClientRect());
 
-        htmlNode.style.display = "block";
-        htmlNode.style.top = `${offsetY + window.scrollY + 10}px`;
-        htmlNode.style.left = `${offsetX}px`;
-
-        clonedRange = range.cloneRange();
+        selectionRef = {
+            parent: parentElement,
+            text: range.toString(),
+            isInput: false,
+            start: range.startOffset,
+            end: range.endOffset,
+        };
     }, 500);
 });
 
@@ -97,22 +148,23 @@ rephraseBtn.addEventListener("click", generateRephrase.bind(rephraseBtn, false))
 fixSpellingBtn.addEventListener("click", generateSpellingFix);
 
 acceptButton.addEventListener("click", () => {
-    const parentElement = clonedRange.startContainer.parentElement;
+    const parentElement = selectionRef.parent;
 
-    if (parentElement) {
-        parentElement.textContent = contentParagraph.textContent;
-        hideToolbar();
+    if (parentElement instanceof HTMLInputElement || parentElement instanceof HTMLTextAreaElement) {
+        const newContent = getNewContent(parentElement.value);
+        parentElement.value = newContent;
     }
+
+    if (parentElement instanceof HTMLElement && parentElement.textContent) {
+        const newContent = getNewContent(parentElement.textContent);
+        parentElement.textContent = newContent;
+    }
+
+    hideToolbar();
 });
 
 discardButton.addEventListener("click", hideToolbar);
 tryAgainButton.addEventListener("click", generateRephrase.bind(rephraseBtn, true));
-
-document.addEventListener("keydown", e => {
-    if (e.code === "Escape" || selection?.type !== "Range") {
-        hideToolbar();
-    }
-});
 
 async function generateRephrase(this: HTMLButtonElement, isRetry: boolean) {
     if (isRetry) {
@@ -124,7 +176,7 @@ async function generateRephrase(this: HTMLButtonElement, isRetry: boolean) {
     contentParagraph.innerHTML = "";
 
     try {
-        const result = await rephrase(clonedRange.toString(), rephraseSelect.value);
+        const result = await rephrase(selectionRef.text, rephraseSelect.value);
 
         for await (const chunk of result.stream) {
             const text = chunk.text();
@@ -149,7 +201,7 @@ async function generateSpellingFix(this: HTMLButtonElement) {
     contentParagraph.innerHTML = "";
 
     try {
-        const result = await fixSpelling(clonedRange.toString());
+        const result = await fixSpelling(selectionRef.text);
 
         for await (const chunk of result.stream) {
             const text = chunk.text();
@@ -171,4 +223,20 @@ function hideToolbar() {
     htmlNode.style.display = "none";
     content.style.display = "none";
     contentParagraph.innerHTML = "";
+}
+
+function showToolbar(rect: DOMRect) {
+    const { bottom: offsetY, left: offsetX } = rect;
+
+    htmlNode.style.display = "block";
+    htmlNode.style.top = `${offsetY + window.scrollY + 10}px`;
+    htmlNode.style.left = `${offsetX}px`;
+}
+
+function getNewContent(content: string): string {
+    return (
+        content.slice(0, selectionRef.start) +
+        contentParagraph.textContent +
+        content.slice(selectionRef.end || content.length - 1, content.length - 1)
+    );
 }
