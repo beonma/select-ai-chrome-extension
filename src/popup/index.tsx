2c5098aaca353ace0696b2a9a313cc6b9c03d330
beonma <hichamelb54@gmail.com>
2025-11-23 22:50:19 +0100

    feat: add first party origin trial tokens

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,16 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import TRIAL_TOKENS from "@src/trial_tokens.json";
-
-document.addEventListener("DOMContentLoaded", () => {
-    TRIAL_TOKENS.forEach(obj => {
-        const otMeta = document.createElement("meta");
-        otMeta.httpEquiv = "origin-trial";
-        otMeta.content = obj.token;
-        document.head.append(otMeta);
-    });
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement);
 
